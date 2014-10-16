b74823c82cdaea62f013d03f8bb8fcb33406045f
iscoop <mkromah@hotmail.co.uk>
2014-10-16 11:57:41 -0700

    ui

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -11,11 +11,11 @@ elsif !Rails.env.development?
 config.storage = :fog
 config.fog_credentials = {
 :provider => 'AWS',
-:aws_access_key_id => ENV['S3_KEY'],
-:aws_secret_access_key => ENV['S3_SECRET'],
+:aws_access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+:aws_secret_access_key => ENV['AWS_SECRET_ACCESS_KEY'],
 :region => 'eu-west-1'
 }
-config.fog_directory = ENV['S3_BUCKET']
+config.fog_directory = ENV['AWS_BUCKET']
 config.fog_public = true
 end
 end
