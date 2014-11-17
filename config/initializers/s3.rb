ce7fb5e494b5e52cc0e8e15c919c337d380c0fbf
iscoop <mkromah@hotmail.co.uk>
2014-11-17 15:40:59 -0800

    debug

--- config/initializers/s3.rb
+++ config/initializers/s3.rb
@@ -1,10 +1,12 @@
 CarrierWave.configure do |config|
-  config.fog_credentials = {
-      :provider               => 'AWS',
-      :aws_access_key_id      => ENV['AWS_ACCESS_KEY_ID'],
-      :aws_secret_access_key  => ENV['AWS_SECRET_ACCESS_KEY'],
-      :region                 => 'eu-west-1'  
+  config.storage    = :aws
+  config.aws_bucket = ENV.fetch('AWS_BUCKET')
+  config.aws_acl    = :public_read
+  config.asset_host = 'http://example.com'
+  config.aws_authenticated_url_expiration = 60 * 60 * 24 * 365
+
+  config.aws_credentials = {
+    access_key_id:     ENV.fetch('AWS_ACCESS_KEY_ID'),
+    secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY')
   }
-  
-  config.fog_directory  = ENV['AWS_BUCKET']
 end
