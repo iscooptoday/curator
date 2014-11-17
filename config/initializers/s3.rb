4fa1551f66198dbf110a28278293dc8ed4171a82
iscoop <mkromah@hotmail.co.uk>
2014-11-17 15:10:14 -0800

    added upload

--- config/initializers/s3.rb
+++ config/initializers/s3.rb
@@ -3,7 +3,8 @@ CarrierWave.configure do |config|
       :provider               => 'AWS',
       :aws_access_key_id      => ENV['AWS_ACCESS_KEY_ID'],
       :aws_secret_access_key  => ENV['AWS_SECRET_ACCESS_KEY']
-      :region                 => 'us-east-1'  
+      :region                 => 'eu-west-1'  
   }
+  
   config.fog_directory  = ENV['AWS_BUCKET']
 end
