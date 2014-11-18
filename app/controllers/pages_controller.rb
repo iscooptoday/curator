fb0c1aa388c258c5ee7e534cad6d3029a494b125
iscoop <mkromah@hotmail.co.uk>
2014-11-18 14:34:10 -0800

    ui

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,6 +4,10 @@ class PagesController < ApplicationController
   def home
   	@link=Link.new
   	@links=Link.all
+
+  	@link_attachment=Link_attachment.new
+  	@link_attachments=Link_attachments.all
+  
   
     end
 
