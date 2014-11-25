5872c0019a4eae6df799ea6aa4de7a2f281809a5
iscoop <mkromah@hotmail.co.uk>
2014-11-24 20:40:31 -0800

    ui

--- config/initializers/s3.rb
+++ config/initializers/s3.rb
@@ -1,6 +1,6 @@
 CarrierWave.configure do |config|
   config.storage    = :aws
-  config.aws_bucket = ENV.fetch('AWS_BUCKET')
+  config.aws_bucket = ENV['AWS_BUCKET']
   config.aws_acl    = :public_read
   config.asset_host = "http://#{ENV['AWS_BUCKET']}.s3.amazonaws.com"
   config.aws_authenticated_url_expiration = 60 * 60 * 24 * 365
