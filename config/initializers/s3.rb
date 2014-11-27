975a89813ca54dd140dd56abb7dc09a4b0703ad6
iscoop <mkromah@hotmail.co.uk>
2014-11-27 00:06:28 -0800

    ui

--- config/initializers/s3.rb
+++ config/initializers/s3.rb
@@ -1,15 +1,9 @@
-CarrierWave.configure do |config|
-  
-  config.storage    = :aws
-  config.aws_bucket = ENV.fetch('AWS_BUCKET')
-  config.aws_acl    = :public_read
-  config.asset_host = "http://#{ENV['AWS_BUCKET']}.s3.amazonaws.com"
-  config.aws_authenticated_url_expiration = 60 * 60 * 24 * 365
 
-  config.aws_credentials = {
-    access_key_id:     ENV.fetch('AWS_ACCESS_KEY_ID'),
-    secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY')
-  }
+ CarrierWave.configure do |config|
+  config.fog_credentials = {
+    :provider               => 'AWS',
+    :aws_access_key_id      => ENV['AWS_ACCESS_KEY_ID'],
+    :aws_secret_access_key  => ENV['AWS_SECRET_ACCESS_KEY']
+
+  config.fog_directory  = ENV['AWS_BUCKET']
 end
-
- 
